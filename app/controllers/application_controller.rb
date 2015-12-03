687e226b4187ea2dc1799740744a74152f221d91
dancinghoneybees <opheliahl@hotmail.com>
2015-12-02 20:35:19 -0500

    add session helper methods in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  helper_method :current_user, :logged_in?
+  
   def index
     # @user = User.all
     # @game = Game.all
     render 'layouts/index'
   end
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !!curent_user
+  end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,17 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(name: params[:session][:name])
     if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
       redirect_to root_url, :notice => "Welcome #{user.name}!"
       else
         flash.now.alert = "Invalid email or password"
         render "new"
     end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url
   end
 
 end
