167b4d92a7a6e5e75ff2d1717183c5ce5ca9ab64
dancinghoneybees <opheliahl@hotmail.com>
2015-12-02 21:18:05 -0500

    add email to user table and email validation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(name: params[:session][:name])
+    user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       redirect_to root_url, :notice => "Welcome #{user.name}!"
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,5 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :name, :password_confirmation
+  validates :email, uniqueness: :true
 end
