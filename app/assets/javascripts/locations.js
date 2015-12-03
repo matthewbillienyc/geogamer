768041ef342cb327c77acb9cd2f14c7318590b63
matthewmain <matthewwmain@gmail.com>
2015-12-03 12:09:10 -0500

    adds-make-a-guess functionality

---
+++ app/assets/javascripts/locations.js
@@ -0,0 +1,12 @@
+
+$(function(){
+	$('#make_guess').on('click', function(){
+		var guess = $('input').val();
+		var answer = $('.name').attr('id')
+		if (guess.toLowerCase() == answer.toLowerCase()) {
+			$('.name').append("<h1> Yes, the answer is "+answer+"</h1>")
+		}
+
+		debugger;
+	})
+})
