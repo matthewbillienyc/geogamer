7a42759b9c40088eda2688ab20dd28f32eed998b
dancinghoneybees <opheliahl@hotmail.com>
2015-12-02 21:05:03 -0500

    delete password column in user table

--- db/schema.rb
+++ db/schema.rb
@@ -54,7 +54,6 @@ ActiveRecord::Schema.define(version: 20151202184026) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "password"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
