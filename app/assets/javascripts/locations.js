94c522bea2784c8a355e0133b0f378494aecc8ce
matthewbillienyc <matthewbillienyc@gmail.com>
2015-12-03 16:28:11 -0500

    stashing local changes

--- app/assets/javascripts/locations.js
+++ app/assets/javascripts/locations.js
@@ -5,6 +5,7 @@ $(function(){
 		var answer = $('.name').attr('id')
 		if (guess.toLowerCase() == answer.toLowerCase()) {
 			$('.name').append("<h1> Yes, the answer is "+answer+"</h1>")
+
 		}
 
 		debugger;
