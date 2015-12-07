4ad285985920c54c005848abb431eb0de2bb1699
matthewmain <matthewwmain@gmail.com>
2015-12-07 14:53:04 -0500

    three locations specs

--- db/migrate/20151203143057_add_status_to_clues_and_locations.rb
+++ db/migrate/20151203143057_add_status_to_clues_and_locations.rb
@@ -1,6 +1,5 @@
 class AddStatusToCluesAndLocations < ActiveRecord::Migration
   def change
     add_column :clues, :status, :string, :default => "unused"
-    add_column :locations, :status, :string, :default => "unused"
   end
 end
