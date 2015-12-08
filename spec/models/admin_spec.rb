d9ab9e2110e54a7e4764919d3bd5c9fe251c75ac
dancinghoneybees <opheliahl@hotmail.com>
2015-12-08 18:40:03 -0500

    add two working admin specs and one in progress

---
+++ spec/models/admin_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe 'Admin' do
+  it 'can access Admin users page' do
+    User.create({name: "admin", email: "admin@admin.com", password: 'admin', password_confirmation: 'admin', admin: true})
+    visit 'login'
+    fill_in('session[email]', with: 'admin@admin.com')
+    fill_in('session[password]', with: 'admin')
+
+    click_button 'Log in'
+    visit '/admin/users'
+    expect(page).to have_link('Delete this user')
+  end
+
+  it 'can delete user' do
+    User.create({name: "jane", email: "jane@jane.com", password: 'jane', password_confirmation: 'jane', admin: false})
+    User.create({name: "admin", email: "admin@admin.com", password: 'admin', password_confirmation: 'admin', admin: true})
+    visit 'login'
+    fill_in('session[email]', with: 'admin@admin.com')
+    fill_in('session[password]', with: 'admin')
+    click_button 'Log in'
+    expect(page).to have_text('jane')
+  end
+
+end
+
+describe 'Admin privilege' do
+  it 'cannot access Admin users page without Admin privilege' do
+    User.create({name: "jane", email: "jane@jane.com", password: 'jane', password_confirmation: 'jane', admin: false})
+    visit 'login'
+    fill_in('session[email]', with: 'jane@jane.com')
+    fill_in('session[password]', with: 'jane')
+
+    click_button 'Log in'
+    visit '/admin/users'
+    expect(page).to have_text('You are tracking a mysterious figure')
+  end
+end
