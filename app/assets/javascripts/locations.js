d68cd33972ba892e1a1af8d00abf94de2b05a84b
matthewbillienyc <matthewbillienyc@gmail.com>
2015-12-10 11:59:08 -0500

    changed correct answer auto moves to next loc

--- app/assets/javascripts/locations.js
+++ app/assets/javascripts/locations.js
@@ -10,8 +10,8 @@ $(function(){
 	})
 
 	$('#make_guess').on('submit', function(e){
-		e.preventDefault();
-		e.stopPropagation();
+		// e.preventDefault();
+		// e.stopPropagation();
 		var guess = $("#name").val();
 		var answer = $('.name').attr('id')
 		var listLength = $('li').length
@@ -19,11 +19,12 @@ $(function(){
 			var score = eval($("#score").attr("value"))
 			score = (score - listLength*10)
 			$("#score").attr("value", score)
-			$('.name').append("<div class='correct-answer-box fade-in-item center-info'><p>Correct!</p><h3>"+answer+"</h3><p class='tiny-text'>You scored "+score+" points this round!</p></div>")
-			$("#guess-div").hide();
-			$('.next-loc-div').fadeIn(3000, 'swing');
-			$('.button-div').fadeOut(3000, 'swing');
-			$('.clues').fadeOut(3000, 'swing');
+			// $('.name').append("<div class='correct-answer-box fade-in-item center-info'><p>Correct!</p><h3>"+answer+"</h3><p class='tiny-text'>You scored "+score+" points this round!</p></div>")
+			// $("#guess-div").hide();
+			// $('.next-loc-div').fadeIn(3000, 'swing');
+			// $('.button-div').fadeOut(3000, 'swing');
+			// $('.clues').fadeOut(3000, 'swing');
+			alert("Correct! You scored "+score+" points!")
 			var url = this.getAttribute('action')
 			var method = this.getAttribute('method')
 			$.ajax({
