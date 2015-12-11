c22ae1f0654f34bc239ce897363b1791aae0606b
yc355 <yifan.i.chen@gmail.com>
2015-12-11 11:18:51 -0500

    added computed attrs to api serializers

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -4,4 +4,12 @@ class UserSerializer < ActiveModel::Serializer
 
   has_many :games
 
+  def attributes
+    attributes = super
+    attributes[:total_number_games] = User.find(attributes[:id]).total_number_games
+    attributes[:total_locations_visited] = User.find(attributes[:id]).total_locations_visited
+    attributes[:total_clues_used] = User.find(attributes[:id]).total_clues_used
+    attributes
+  end
+
 end
