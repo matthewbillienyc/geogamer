354ac71516ba3ee8716b02879ed7fae83a86289e
matthewbillienyc <matthewbillienyc@gmail.com>
2015-12-14 09:15:40 -0500

    ember commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   mount_ember_app :frontend, to: "/"
 
   namespace :api do
--- frontend/app/models/user.js
+++ frontend/app/models/user.js
@@ -3,8 +3,8 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 	name: DS.attr('string'),
 	email: DS.attr('string'),
-	total_number_games: DS.attr('integer'),
-	total_locations_visited: DS.attr('integer'),
-	total_clues_used: DS.attr('integer'),
-	// games: DS.hasMany('games', {async: true})			
+	total_number_games: DS.attr('string'),
+	total_locations_visited: DS.attr('string'),
+	total_clues_used: DS.attr('string'),
+	// games: DS.hasMany('games', {async: true})
 });
